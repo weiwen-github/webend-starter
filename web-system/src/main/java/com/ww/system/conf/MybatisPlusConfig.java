73d8995f0615e2ce6508fb4b7d65909f4cb27c21
weiwen <weiwen@xwmail.com>
2020-11-27 18:02:47 +0800

    fix: add sum function

--- web-system/src/main/java/com/ww/system/conf/MybatisPlusConfig.java
+++ web-system/src/main/java/com/ww/system/conf/MybatisPlusConfig.java
@@ -14,7 +14,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfig {
 
-  /** 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题 */
+  /**
+   * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
+   * DbType.MYSQL 设置为使用的数据库类型
+   */
   @Bean
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
